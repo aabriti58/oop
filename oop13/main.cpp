54f846e64da9853c4a3fa115288f7854dfef95d7
aabriti58 <55425391+aabriti58@users.noreply.github.com>
2021-03-14 17:14:34 +0545

    Add files via upload

---
+++ oop13/main.cpp
@@ -0,0 +1,132 @@
+//Primitive to user defined data types
+#include <iostream>
+using namespace std;
+class Complex{
+private:
+    int a,b;
+public:
+    Complex(){}
+    Complex(int k){
+        a = k;
+        b = 0;
+    }
+    void setdata(int x, int y){
+        a=x;
+        b=y;
+    }
+    void showData(){
+        cout<<"a:"<<a<<"b:"<<b<<endl;
+    }
+};
+int main(){
+    Complex c1;
+    c1.setdata(10,20);
+    c1.showData();
+    int x = 5;
+    c1 = x;
+    c1.showData();
+    return 0;
+}
+
+
+/*//User defined to primitive data types
+#include<iostream>
+using namespace std;
+class Complex{
+private:
+    int a,b;
+public:
+    void setData(int x, int y){
+        a=x;
+        b=y;
+    }
+    void showData(){
+        cout<<"a:"<<a<<"b:"<<b<<endl;
+    }
+    operator int(){ //casting operator
+        return (a);
+    }
+};
+int main(){
+    Complex c1;
+    c1.setData(3,5);
+    c1.showData();
+    int x;
+    x = c1;
+    cout<<"After type conversion: "<<x<<endl;
+    return 0;
+}*/
+
+/*//User-defined to User-defined
+#include<iostream>
+using namespace std;
+class one{
+    int code,items;
+    float price;
+public:
+    one(int c, int i, int p){
+        code = c;
+        items = i;
+        price = p;
+    }
+    void showData(){
+        cout<<"\n CODE= "<<code;
+        cout<<"\n ITEMS= "<<items;
+        cout<<"\n VALUE= "<<price;
+    }
+    int getcode()
+    {
+        return code;
+    }
+    int getitems()
+    {
+        return items;
+    }
+    int getprice()
+    {
+        return price;
+    }
+    operator float(){ //class to basic conversion, returns float value
+        return items*price;
+    }
+};
+class two
+{
+    int code;
+    float value;
+public:
+    two()
+    {
+        code = 0;
+        value = 0;
+    }
+    two(int x, float y)
+    {
+        code = x;
+        value = y;
+    }
+    void showData()
+    {
+        cout<<"\n CODE= "<<code;
+        cout<<"\n VALUE= "<<value;
+    }
+    two(one obj1)
+    {
+        code = obj1.getcode();
+        value = obj1.getitems()*obj1.getprice();
+    }
+
+};//end of class two
+int main(){
+    one ob1(1000,20,150);
+    float tot_value;
+    two ob2;
+    tot_value = ob1; //invokes operator function of class one
+    ob2 = one(ob1); //conversion from class two to class one
+    cout<<"\n item from class one ";
+    ob1.showData();
+    cout<<"\n VALUE= "<<tot_value;
+    cout<<"item from class two ";
+    ob2.showData();
+
+}*/
